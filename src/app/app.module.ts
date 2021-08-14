55668b7cd54229a3a288d7e79e9d48e6cdd3ab3a
Dorian Aldair Oliveros 0909-17-8081 <aldair_oliveros09@hotmail.es>
2021-08-14 11:40:39 -0600

    Creando Contenedor Principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { ContenedorPrincipalComponent } from './components/contenedor-principal/contenedor-principal.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContenedorPrincipalComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
