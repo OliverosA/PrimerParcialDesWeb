43f417d599559a972099049fcf39a62adce84719
Dorian Aldair Oliveros 0909-17-8081 <aldair_oliveros09@hotmail.es>
2021-08-14 19:33:14 -0600

    Insertando elementos mediante bucle en contenedor lateral

--- src/app/components/contenedor-lateral/contenedor-lateral.component.ts
+++ src/app/components/contenedor-lateral/contenedor-lateral.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContenedorLateralComponent implements OnInit {
 
+  elementos=[
+    {
+      imagen: "assets/Images/angular-bg.png",
+      nombre: "¿Qué Es Angular?",
+      descripcion: "Este elemento tiene una descripcion"
+    },
+    {
+      imagen: "assets/Images/laravel.png",
+      nombre: "¿Qué Es Laravel?",
+      descripcion: "Este elemento tiene una descripcion"
+    },
+    {
+      imagen: "assets/Images/ionic.png",
+      nombre: "¿Qué Es Ionic?",
+      descripcion: "Este elemento tiene una descripcion"
+    },
+    {
+      imagen: "assets/Images/bootstrap.png",
+      nombre: "¿Qué Es Bootstrap?",
+      descripcion: "Este elemento tiene una descripcion"
+    }
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
